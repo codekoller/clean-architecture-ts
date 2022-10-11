b7a20ca3a421c9da28e4556a913fc4fabd698ee3
William Koller <williamkoller404@gmail.com>
2022-10-11 14:15:25 -0300

    test: :white_check_mark: should be update methods

--- src/tests/domain/entities/account/account.entity.spec.ts
+++ src/tests/domain/entities/account/account.entity.spec.ts
@@ -26,4 +26,70 @@ describe('Account Test', () => {
       ...accountProps,
     });
   });
+
+  it('should be updateName method', () => {
+    let accountProps: AccountProps = {
+      name: 'any_name',
+      surname: 'any_surname',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    };
+    let account = AccountEntity.create(accountProps);
+    account.updateName('any_is_name');
+    expect(account.name).toBe('any_is_name');
+  });
+
+  it('should be updateSurname method', () => {
+    let accountProps: AccountProps = {
+      name: 'any_name',
+      surname: 'any_surname',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    };
+    let account = AccountEntity.create(accountProps);
+    account.updateSurname('any_is_surname');
+    expect(account.surname).toBe('any_is_surname');
+  });
+
+  it('should be updateEmail method', () => {
+    let accountProps: AccountProps = {
+      name: 'any_name',
+      surname: 'any_surname',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    };
+    let account = AccountEntity.create(accountProps);
+    account.updateEmail('any_email_updated@mail.com');
+    expect(account.email).toBe('any_email_updated@mail.com');
+  });
+
+  it('should be updatePassword method', () => {
+    let accountProps: AccountProps = {
+      name: 'any_name',
+      surname: 'any_surname',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    };
+    let account = AccountEntity.create(accountProps);
+    account.updatePassword('any_password_updated');
+    expect(account.password).toBe('any_password_updated');
+  });
+
+  it('should be toJSON()', () => {
+    let accountProps: AccountProps = {
+      name: 'any_name',
+      surname: 'any_surname',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    };
+    let account = AccountEntity.create(accountProps);
+    account.toJSON();
+    expect(account.toJSON()).toStrictEqual({
+      id: account.id,
+      name: 'any_name',
+      surname: 'any_surname',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    });
+  });
 });
