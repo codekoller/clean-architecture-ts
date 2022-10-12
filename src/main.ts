893675b05377035201d896107c89a6f4c3ab2812
William Koller <williamkoller404@gmail.com>
2022-10-12 02:09:27 -0300

    feat: :sparkles: add prt in main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@app/app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get(ConfigService);
+
+  const port = config.get<number>('PORT');
+  await app.listen(port, () => logger.log(`Server listening at port: ${port}`));
 }
 bootstrap();
