f6c71df271aa1c6c8b7dd0556c337cd4c25edbfb
William Koller <williamkoller404@gmail.com>
2022-10-12 00:18:45 -0300

    chore: :wrench: update jest config

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
     '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/main.ts',
     '!<rootDir>/src/app.module.ts',
+    '!<rootDir>/src/infra/config/environment-config/environment-config.module.ts',
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
