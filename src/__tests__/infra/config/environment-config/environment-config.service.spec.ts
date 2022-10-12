79836b13b931752a4ff1fe92e0889c487747456b
William Koller <williamkoller404@gmail.com>
2022-10-12 00:20:25 -0300

    test: :white_check_mark: should be get database uri

---
+++ src/__tests__/infra/config/environment-config/environment-config.service.spec.ts
@@ -0,0 +1,23 @@
+import { EnvironmentConfigService } from '@app/infra/config/environment-config/environment-config.service';
+import { CreateMockEnvironmentConfig } from './mocks/create-mock-environment-config';
+
+describe('EnvironmentConfigService', () => {
+  let service: EnvironmentConfigService;
+
+  beforeEach(async () => {
+    const module = await CreateMockEnvironmentConfig();
+
+    service = module.service;
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should be get database uri', () => {
+    const databaseUri = service.getDatabaseUri();
+    expect(databaseUri).toStrictEqual(
+      'mongodb://cleanarch:27017/clean-database',
+    );
+  });
+});
