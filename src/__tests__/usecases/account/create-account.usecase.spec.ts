e203bc0ebab7656eae43a6155b5683da300ae85f
William Koller <williamkoller404@gmail.com>
2022-10-11 23:44:32 -0300

    test: :white_check_mark: should be create a new account

---
+++ src/__tests__/usecases/account/create-account.usecase.spec.ts
@@ -0,0 +1,23 @@
+import { AccountInMemoryRepository } from '@app/infra/db/inmemory/account-inmemory.repository';
+import { CreateAccountUseCase } from '@app/usecases/account/create-account.usecase';
+
+describe('CreateAccountUseCase Test', () => {
+  it('should be create a new account', async () => {
+    const repository = new AccountInMemoryRepository();
+    const createAccountUseCase = new CreateAccountUseCase(repository);
+    const output = await createAccountUseCase.execute({
+      name: 'any_name',
+      surname: 'any_surname',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    });
+    expect(repository.acccounts).toHaveLength(1);
+    expect(output).toStrictEqual({
+      id: repository.acccounts[0].id,
+      name: 'any_name',
+      surname: 'any_surname',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    });
+  });
+});
